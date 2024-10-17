980a350ec7c3402c7c1b96bc927c56d23889ba98
alokshandilya <alok.shandilya10@gmail.com>
2024-10-17 11:17:03 +0530

    feat(models): Add Trip and Note models
    Add Trip and Note models to the trip app. The Trip model has fields for
    city, country, start_date, end_date, and owner(fk). The Note model has
    fields for trip(fk), description, type, img, and rating.
    
    - install Pillow for image field support
      - https://docs.djangoproject.com/en/5.1/howto/static-files/#serving-static-files-during-development
    - makemigrations and migrate
    - register Trip and Note models in the admin site

--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Trip(models.Model):
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=2)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="trips",  # access trips from user instance (user.trips)
+    )
+
+    def __str__(self):
+        return self.city
+
+
+class Note(models.Model):
+    EXCURSIONS = (
+        ("event", "Event"),
+        ("dining", "Dining"),
+        ("experience", "Experience"),
+        ("general", "General"),
+    )
+
+    trip = models.ForeignKey(
+        Trip,
+        on_delete=models.CASCADE,
+        related_name="notes",  # access notes from trip instance (trip.notes)
+    )
+    description = models.TextField()
+    type = models.CharField(max_length=100, choices=EXCURSIONS)
+    img = models.ImageField(upload_to="notes", blank=True, null=True)
+    rating = models.PositiveIntegerField(
+        default=1,
+        validators=[MaxValueValidator(5)],
+    )
+
+    def __str__(self):
+        return f"{self.name} in {self.trip.city}"
