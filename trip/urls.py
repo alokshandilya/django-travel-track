c8e9de715f6698a86c6aa236f776cd3a0b7b2ca7
alokshandilya <alok.shandilya10@gmail.com>
2024-10-17 11:36:34 +0530

    feat: Add trip list view
    - add a new view to list all trips. The view is accessible at /dashboard/

--- trip/urls.py
+++ trip/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, trip_list
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("dashboard/", trip_list, name="trip-list"),
 ]
--- trip/views.py
+++ trip/views.py
@@ -1,6 +1,16 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import Trip
 
-# Create your views here.
+
 class HomeView(TemplateView):
     template_name = "trip/index.html"
+
+
+def trip_list(request):
+    trips = Trip.objects.all()
+    context = {
+        "trips": trips,
+    }
+    return render(request, "trip/trip_list.html", context)
